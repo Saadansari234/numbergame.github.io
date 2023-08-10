00ca6c52e81b3d88a3cc5c78cec8f812265a6084
Saadansari234 <saadansari3212@gmail.com>
2023-08-10 14:42:10 +0530

    added new code

--- numbergame2.js
+++ numbergame2.js
@@ -17,75 +17,180 @@
 // }
 
 
-let number = [Math.floor(Math.random() * 10) + 1]
-let attempt = 5 ;
+// let number = [Math.floor(Math.random() * 10) + 1]
+// let attempt = 5 ;
 
 
-/*hh
-hh*/
+// /*hh
+// hh*/
 
 
   
+// function handlebtn() {
+//  var submitbutton= document.getElementById("submitbtn");
+//   let input = document.getElementById("input").value;
+//   let output = document.getElementById("output");
+//   let resultmessage = document.getElementById("resultmessage");
+//   let attemtscounter= document.getElementById("attemtscounter");
+
+
+  
+
+//   //let give condition to the input
+
+//   if (input == number) {
+//     output.innerHTML = `you guess right , your answer was ${number}`;
+//     submitbutton.disabled= true;
+//     resultmessage.innerHTML = `<h1>🌹</h1>`;
+//     return;
+//   } else if (input < number) {
+//     attempt--;
+//     if (attempt === 0 ) {
+//       resultmessage.innerHTML = `better luck next time. the right number was ${number}`;
+//       attemtscounter.innerHTML = attempt;
+//       submitbutton.disabled= true;
+//       return;
+      
+//     } else {
+//       output.innerHTML = 'your number is too low!';
+//       attemtscounter.innerHTML = attempt;
+//     }
+//     console.log("attempt");
+   
+//   } else if(input > number) {
+//     attempt--;
+//     if (attempt === 0 ) {
+//       resultmessage.innerHTML = `better luck next time. the right number was ${number}`;
+//       attemtscounter.innerHTML = attempt;
+//       submitbutton.disabled= true;
+//       return;
+
+//     } else {
+//       output.innerHTML = 'your number is too high!';
+//       attemtscounter.innerHTML = attempt;
+//     }
+//     console.log("attempt");
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let number = Math.floor(Math.random() * 10) + 1;
+let attempt = 5;
+
 function handlebtn() {
- var submitbutton= document.getElementById("submitbtn");
+  var submitbutton = document.getElementById("submitbtn");
   let input = document.getElementById("input").value;
   let output = document.getElementById("output");
   let resultmessage = document.getElementById("resultmessage");
-  let attemtscounter= document.getElementById("attemtscounter");
+  let attemtscounter = document.getElementById("attemtscounter");
 
-
-  
-
-  //let give condition to the input
-
-  if (input == number) {
-    output.innerHTML = `you guess right , your answer was ${number}`;
-    submitbutton.disabled= true;
+  if (parseInt(input) === number) {
+    output.innerHTML = `You guessed right! Your answer was ${number}`;
+    submitbutton.disabled = true;
     resultmessage.innerHTML = `<h1>🌹</h1>`;
     return;
-  } else if (input < number) {
+  } else if (parseInt(input) < number) {
     attempt--;
-    if (attempt === 0 ) {
-      resultmessage.innerHTML = `better luck next time. the right number was ${number}`;
+    if (attempt === 0) {
+      resultmessage.innerHTML = `Better luck next time. The right number was ${number}`;
       attemtscounter.innerHTML = attempt;
-      submitbutton.disabled= true;
+      submitbutton.disabled = true;
       return;
-      
     } else {
-      output.innerHTML = 'your number is too low!';
+      output.innerHTML = 'Your number is too low!';
       attemtscounter.innerHTML = attempt;
     }
-    console.log("attempt");
-   
-  } else if(input > number) {
+  } else if (parseInt(input) > number) {
     attempt--;
-    if (attempt === 0 ) {
-      resultmessage.innerHTML = `better luck next time. the right number was ${number}`;
+    if (attempt === 0) {
+      resultmessage.innerHTML = `Better luck next time. The right number was ${number}`;
       attemtscounter.innerHTML = attempt;
-      submitbutton.disabled= true;
+      submitbutton.disabled = true;
       return;
-
     } else {
-      output.innerHTML = 'your number is too high!';
+      output.innerHTML = 'Your number is too high!';
       attemtscounter.innerHTML = attempt;
     }
-    console.log("attempt");
-
-
-    
-
   }
-
-  
-    // Here you can perform any additional tasks when the loop stops
-    // For example, disable the input field or hide the submit button
-   
-
-
-//  if (input > number) {
-  //}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
